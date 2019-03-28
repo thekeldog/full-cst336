7fd6714dc3be4f63b56ba46e3f162a2d0e962a7a
Kellen Rice <thekeldog@gmail.com>
2019-03-28 03:48:22 +0000

    changed connection info for Heroku

--- labs/otter-mart/api/connection.php
+++ labs/otter-mart/api/connection.php
@@ -5,6 +5,23 @@ function get_database_connection($dbname = "ottermart") {
     $user_name = 'root';
     $password = "";
     
+    /*
+    Host 	bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com 	
+    Username 	sf1qzp17uzk5ms6f 	
+    Password 	p6n7qso52n2og7b7 	
+    Port 	3306 	
+    Database 	lbkixx7usdyvo8hh
+    */
+    
+    if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("mysql://sf1qzp17uzk5ms6f:p6n7qso52n2og7b7@bfjrxdpxrza9qllq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lbkixx7usdyvo8hh"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+
+    
     //create db connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $user_name, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
