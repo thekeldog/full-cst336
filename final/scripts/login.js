35b00d895c491aec2d30c80f4f763bb9cc7bde23
Kellen Rice <thekeldog@gmail.com>
2019-05-14 01:36:49 +0000

    login trouble

--- final/scripts/login.js
+++ final/scripts/login.js
@@ -7,30 +7,30 @@ $(document).ready(function(){
       let userPassword = $('#userPassword').val();
       console.log(userName+userPassword)
       
-       $.ajax({
-                type: "GET",
-                url: "api/user_login.php",
-                dataType: "json",
-                data: {
-                    "userName": userName,
-                    "passWord": userPassword
-                },
-                success: function(data) {
-                    if(data['success'] === true){
-                        console.log("you logged in you savage!")
-                        window.location.href = 'index.php'
-                    }else{
-                        console.log("you messed something up, bro")
-                    }
+    //   $.ajax({
+    //             type: "GET",
+    //             url: "api/user_login.php",
+    //             dataType: "json",
+    //             data: {
+    //                 "userName": userName,
+    //                 "passWord": userPassword
+    //             },
+    //             success: function(data) {
+    //                 if(data['success'] === true){
+    //                     console.log("you logged in you savage!")
+    //                     window.location.href = 'index.php'
+    //                 }else{
+    //                     console.log("you messed something up, bro")
+    //                 }
 
-                },
-                error: function(data){
-                  console.log("that didn't go well");  
-                },
-                complete: function(data, status) { //optional, used for debugging purposes
-                    //console.log(status);
-                }
-            });
+    //             },
+    //             error: function(data){
+    //               console.log("that didn't go well");  
+    //             },
+    //             complete: function(data, status) { //optional, used for debugging purposes
+    //                 //console.log(status);
+    //             }
+    //         });
       
    });
    
